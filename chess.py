7f899be5ca0a938bc1269a362394f78b70c3ff0c
Bmeshanko <bmeshank@purdue.edu>
2022-04-23 00:35:48 -0400

    Working on Chess Interface

--- chess.py
+++ chess.py
@@ -1,39 +1,59 @@
 import tkinter as tk
 
+class DragManager():
+	def add_dragable(self, widget):
+		widget.bind("<ButtonPress-1>", self.on_start)
+		widget.bind("<B1-Motion>", self.on_drag)
+		widget.bind("<ButtonRelease-1>", self.on_drop)
+		widget.configure(cursor="hand1")
+	
+	def on_start(self, event):
+		pass
+	def on_drag(self, event):
+		pass
+	def on_drop(self, event):  
+		x,y = event.widget.winfo_pointerxy()
+		target = event.widget.winfo_containing(x,y)
+		try:
+			target.configure(image=event.widget.cget("image"))
+		except:
+			pass
+
 class GameBoard(tk.Frame):
-    def __init__(self, parent, rows=8, columns=8, size=32, color1="#EEEED5", color2="#7D945D"):
-        '''size is the size of a square, in pixels'''
+	def __init__(self, parent, rows=8, columns=8, size=32, color1="#EEEED5", color2="#7D945D"):
+		'''size is the size of a square, in pixels'''
 
-        self.rows = rows
-        self.columns = columns
-        self.size = size
-        self.color1 = color1
-        self.color2 = color2
-        self.pieces = {}
+		self.rows = rows
+		self.columns = columns
+		self.size = size
+		self.color1 = color1
+		self.color2 = color2
+		self.pieces = {}
 
-        canvas_width = columns * size
-        canvas_height = rows * size
+    	canvas_width = columns * size
+    	canvas_height = rows * size
 
-        tk.Frame.__init__(self, parent)
-        self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0,
-                                width=canvas_width, height=canvas_height, background="bisque")
-        self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
+    	tk.Frame.__init__(self, parent)
+    	self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0,
+    	                        width=canvas_width, height=canvas_height, background="bisque")
+    	self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
 
-        # this binding will cause a refresh if the user interactively
-        # changes the window size
-        self.canvas.bind("<Configure>", self.refresh)
+    	# this binding will cause a refresh if the user interactively
+    	# changes the window size
+    	self.canvas.bind("<Configure>", self.refresh)
 
-    def addpiece(self, name, image, row, column):
-        '''Add a piece to the playing board'''
-        self.canvas.create_image(0,0, image=image, tags=(name, "piece"), anchor="c")
-        self.placepiece(name, row, column)
+	def addpiece(self, name, image, row, column):
+    	'''Add a piece to the playing board'''
+    	#self.canvas.create_image(0,0, image=image, tags=(name, "piece"), anchor="c")
+		self.pack()
+    	self.placepiece(name, row, column)
 
-    def placepiece(self, name, row, column):
-        '''Place a piece at the given row/column'''
-        self.pieces[name] = (row, column)
-        x0 = (column * self.size) + int(self.size/2)
-        y0 = (row * self.size) + int(self.size/2)
-        self.canvas.coords(name, x0, y0)
+	def placepiece(self, name, row, column):
+    	'''Place a piece at the given row/column'''
+    	self.pieces[name] = (row, column)
+    	x0 = (column * self.size) + int(self.size/2)
+    	y0 = (row * self.size) + int(self.size/2)
+    	self.canvas.coords(name, x0, y0)
 
     def refresh(self, event):
         '''Redraw the board, possibly in response to window being resized'''
@@ -82,9 +102,12 @@ imagedata = '''R0lGODlhEAAQAOeSAKx7Fqx8F61/G62CILCJKriIHM+HALKNMNCIANKKANOMALuRK
 
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    board = GameBoard(root)
-    board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
-    player1 = tk.PhotoImage(data=imagedata)
-    board.addpiece("player1", player1, 2,2)
-    root.mainloop()
+	root = tk.Tk()
+	board = GameBoard(root)
+	dm = DragManager();
+	board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+	player1 = tk.Label(text="P")
+	#player1 = tk.PhotoImage(data=imagedata)
+	dm.add_dragable(player1)
+	board.addpiece("player1", player1, 2,2)
+	root.mainloop()
