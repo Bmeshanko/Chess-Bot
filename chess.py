fe93b9d9fff64d7382cde94c895a4c8c9e2dc820
Bmeshanko <bmeshank@purdue.edu>
2022-04-23 09:49:50 -0400

    Fixing issues with the board not correctly loading.

--- chess.py
+++ chess.py
@@ -39,6 +39,30 @@ class GameBoard(tk.Frame):
 		self.canvas = tk.Canvas(self, borderwidth=0, highlightthickness=0,
     	                        width=canvas_width, height=canvas_height, background="bisque")
 		self.canvas.pack(side="top", fill="both", expand=True, padx=2, pady=2)
+		self.canvas.bind("<Configure>", self.refresh)
+
+	def refresh(self, event):
+		'''Redraw the board, possibly in response to window being resized'''
+		xsize = int((event.width-1) / self.columns)
+		ysize = int((event.height-1) / self.rows)
+		self.size = min(xsize, ysize)
+		self.canvas.delete("square")
+		color = self.color2
+		for row in range(self.rows):
+			color = self.color1 if color == self.color2 else self.color2
+			for col in range(self.columns):
+				x1 = (col * self.size)
+				y1 = (row * self.size)
+				x2 = x1 + self.size
+				y2 = y1 + self.size
+				self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill=color, tags="square")
+				color = self.color1 if color == self.color2 else self.color2
+
+		for name in self.pieces:
+			self.placepiece(name, self.pieces[name][0], self.pieces[name][1])
+
+		self.canvas.tag_raise("piece")
+		self.canvas.tag_lower("square")
 
 	def addpiece(self, name, image, row, column):
 		'''Add a piece to the playing board'''
